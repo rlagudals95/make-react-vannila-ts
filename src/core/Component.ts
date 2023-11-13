776cab83a7a88f14b82d7e2f252bae361320832e
hm.kim <hm.kim@ohoolabs.com>
2023-11-13 23:42:57 +0900

    chore: 기능 주석 추가

--- src/core/Component.ts
+++ src/core/Component.ts
@@ -1,33 +1,39 @@
-import { updateRealDOM } from '../index';
+import { updateRealDOM } from "../index";
 
+// Props와 State의 타입을 정의하는 인터페이스
 export interface PropsType {}
 export interface StateType {}
 
+// Partial 타입을 재정의하여 모든 속성을 선택적으로 만듦
 type Partial<T> = {
   [P in keyof T]?: T[P];
 };
+
+// 컴포넌트의 고유한 식별자 타입 정의
 export type ComponentId = string;
 
-const TAG: string = 'C-';
+// 컴포넌트 클래스 시작
+const TAG: string = "C-";
 
 export default class Component<
-  P extends PropsType = PropsType,
-  S extends StateType = StateType,
+  P extends PropsType = PropsType, // Props의 기본값은 PropsType
+  S extends StateType = StateType // State의 기본값은 StateType
 > {
   props: P;
   state: S;
 
-  id: ComponentId = TAG + Component.ID;
-  $dom: Element;
+  id: ComponentId = TAG + Component.ID; // 컴포넌트 인스턴스마다 고유한 ID 생성
+  $dom: Element; // 실제 DOM 엘리먼트
 
-  static ID: number = 0;
-  shouldUpdate = true;
+  static ID: number = 0; // 컴포넌트의 ID 초기값
+
+  shouldUpdate = true; // 업데이트 여부를 나타내는 플래그
 
   constructor(props: P) {
-    ++Component.ID;
+    ++Component.ID; // 컴포넌트가 생성될 때마다 ID 증가
     this.props = props;
-    this.state = {} as S;
-    this.$dom = document.createElement('code');
+    this.state = {} as S; // 초기 state를 빈 객체로 설정
+    this.$dom = document.createElement("code"); // 가상 DOM 엘리먼트 생성
   }
 
   willMount() {}
@@ -35,22 +41,26 @@ export default class Component<
   didMount() {}
   didUpdate() {}
 
+  // DOM을 설정하는 메서드
   setDom(): void {
     this.willMount();
-    this.$dom = this.render() as Element;
+    this.$dom = this.render() as Element; // 렌더링 결과를 DOM에 할당
     this.didMount();
   }
 
+  // 렌더링 메서드. 자식 클래스에서 구현되어야 함
   render(): HTMLElement | ChildNode | DocumentFragment {
-    throw new Error('need to be implemented');
+    throw new Error("need to be implemented");
   }
 
+  // DOM 업데이트 메서드
   updateDOM(): void {
-    const $copy = this.render();
-    this.$dom.parentNode?.replaceChild($copy, this.$dom);
-    this.$dom = $copy as Element;
+    const $copy = this.render(); // 새로운 렌더링 결과를 얻음
+    this.$dom.parentNode?.replaceChild($copy, this.$dom); // 이전 DOM을 새로운 DOM으로 교체
+    this.$dom = $copy as Element; // 새로운 DOM으로 업데이트
   }
 
+  // 전체 업데이트 메서드
   update(): void {
     this.willMount();
     this.willUpdate();
@@ -58,13 +68,14 @@ export default class Component<
     this.didUpdate();
   }
 
+  // State를 업데이트하고 실제 DOM에 반영
   setState(newState: Partial<S>) {
     const nextState = { ...this.state, ...newState };
     if (JSON.stringify(this.state) === JSON.stringify(nextState)) {
-      return;
+      return; // 새로운 State와 현재 State가 같으면 업데이트를 수행하지 않음
     }
-    this.state = nextState;
-    this.update();
-    updateRealDOM();
+    this.state = nextState; // 새로운 State로 업데이트
+    this.update(); // 전체 업데이트 수행
+    updateRealDOM(); // 실제 DOM 업데이트
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -17,18 +17,26 @@ const routes: Route[] = [
 ];
 
 const $app = document.querySelector("#app");
+// 가상 DOM을 기반으로 실제 DOM을 업데이트하는 함수
 export const updateRealDOM = () => {
+  // requestAnimationFrame을 사용하여 다음 프레임에 업데이트를 예약
   window.requestAnimationFrame(() => {
+    // $app이 존재하면 diff 함수를 호출하여 가상 DOM과 실제 DOM 간의 차이를 계산하고 업데이트 수행
     if ($app) diff(document.body, $app, window.virtualDOM);
   });
 };
 
+// $app이 존재하는 경우 초기화 및 라우터 설정을 수행
 if ($app) {
+  // window.virtualDOM에 새로운 div 엘리먼트를 생성하여 할당
   window.virtualDOM = document.createElement("div");
+  // virtualDOM에 "app"이라는 id를 부여
   window.virtualDOM.id = "app";
+  // initRouter 함수를 호출하여 라우터 초기화. $app과 라우터 설정(routes)을 전달
   initRouter({ $app: window.virtualDOM, routes });
 }
 
+// "forceRender" 이벤트가 발생할 때마다 updateRealDOM 함수를 호출하는 이벤트 리스너 등록
 window.addEventListener("forceRender", () => {
   updateRealDOM();
 });
